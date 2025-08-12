8def16573309dc103520a81634bad30d6b95fa7e
wahoo78 <60235037+wahoo78@users.noreply.github.com>
2025-08-12 10:58:38 -0400

    Update streamlit_app.py to add warehouse

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ name_on_order = st.text_input('Name on smoothie:', '')
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+session.sql("USE WAREHOUSE COMPUTE_WS").collect()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # st.dataframe(data=my_dataframe, use_container_width=True)
